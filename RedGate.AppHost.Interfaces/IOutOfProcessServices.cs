80a370a5713e3b203c74a414e478eb04214951a7
Andrew Hunter <andrew.hunter@red-gate.com>
2014-09-23 17:33:29 +0100

    Add an interface that can be used to provide services from the client to the server (in the same way that IAppHostServices does the opposite)

---
+++ RedGate.AppHost.Interfaces/IOutOfProcessServices.cs
@@ -0,0 +1,14 @@
+﻿namespace RedGate.AppHost.Interfaces
+{
+    /// <summary>
+    /// Assemblies may provide an object that implements this method in order to provide services to the host task
+    /// </summary>
+    public interface IOutOfProcessServices
+    {
+        /// <summary>
+        /// Returns a service object of type TServiceType.
+        /// </summary>
+        TServiceType GetService<TServiceType>(IAppHostServices services)
+            where TServiceType: class;
+    }
+}
